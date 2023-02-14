ca350a9e32e0a8262b5f843fbef8f8b626319be0
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-02-14 14:37:49 +0000

    Salvar

--- Operadores.java
+++ Operadores.java
@@ -4,12 +4,12 @@ public class Operadores {
         int x = 9;
         int y = 2;
 
-        System.out.printf("x = y = %d\n", x = y);
-        System.out.printf("x - y = %d\n", x - y);
-        System.out.printf("x * y = %d\n", x * y);
-        System.out.printf("x / y = %d\n", x + y); //divisão inteira
-        System.out.printf("x / y = %2.2f\n", x / (double)y); //divisão normal
-        System.out.printf("x %% y = %d\n", x % y);
+        System.out.printf("x = y = %d \n", x = y);
+        System.out.printf("x - y = %d \n", x - y);
+        System.out.printf("x * y = %d \n", x * y);
+        System.out.printf("x / y = %d \n", x + y); //divisão inteira
+        System.out.printf("x / y = %2.2f \n", x / (double)y); //divisão normal
+        System.out.printf("x %% y = %d \n", x % y);
 
     }
 }
