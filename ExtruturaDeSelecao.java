cb9a189ebdf846a0c14c6559b7f911dbafa740b5
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-02-14 14:23:35 +0000

    Salvar

--- ExtruturaDeSelecao.java
+++ ExtruturaDeSelecao.java
@@ -7,10 +7,10 @@ public class ExtruturaDeSelecao {
 
        if (idade >= 18)
        {
-        System.out.print("Apto ao Serviço Militar Obrigatorio! ＼(●~▽~●)");
+        System.out.print("Apto ao Serviço Militar Obrigatorio! (づ｡◕‿‿◕｡)づ ");
     
        } else {
-            System.out.print("Não esta apto ao Serviço Militar Obrigatorio! (︶︹︺) ");
+            System.out.print("Não esta apto ao Serviço Militar Obrigatorio! (◕﹏◕✿) ");
        }
     }
 }
