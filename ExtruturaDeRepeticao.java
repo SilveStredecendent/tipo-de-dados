2a7eb5dba750b0a7a646db2d4fa06c1082ef4111
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-02-14 13:58:11 +0000

    Salvar

--- ExtruturaDeRepeticao.java
+++ ExtruturaDeRepeticao.java
@@ -1,3 +1,11 @@
 public class ExtruturaDeRepeticao {
+    public static void main(String[] args){
+
+        int cc = 0;
+        while( cc<10){
+            System.out.print("teste \n" + (cc+10));
+            cc++;
+        }
+    }
     
 }
