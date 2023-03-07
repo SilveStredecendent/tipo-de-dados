cd26112dbddb737119cd1c590c9c8a77e9a6fda9
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-03-07 14:31:58 +0000

    salvar

--- DiasDaSemana.java
+++ DiasDaSemana.java
@@ -1,44 +1,51 @@
+import java.util.Scanner;
 public class DiasDaSemana {
-    public static void main(String[] args){
+        public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        double PS1, AL2, IMC;
+        int semana;
         
-        System.out.println("Informe seu pesso: ");
-        PS1 = input.nextFloat();
-
-        System.out.println("Informe sua altura: ");
-        AL2 = input.nextFloat();
-
-
-        if ()
+        System.out.println("Informe o número referente ao dia da semana");
+        semana = input.nextInt();
+        
+        if (semana == 1)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Domingo \n");
         }
     
-        else if ()
+        else if (semana == 2)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Segunda-feira. \n");
         }
     
-        else if()
+        else if(semana == 3)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Terça-feira. \n");
         }
     
-        else if()
+        else if(semana == 4)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Quarta-feira. \n");
         }
 
-        else if()
+        else if(semana == 5)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Quinta-feira. \n");
         }
 
-        else if()
+        else if(semana == 6)
         {
-            System.out.print( + IMC + ", o dia da semana correspondente é \n");
+            System.out.printf( + semana + ", o dia da semana correspondente a Sexta-feira. \n");
+        }
+
+        else if(semana == 7)
+        {
+            System.out.printf( + semana + ", o dia da semana correspondente a Sábado. \n");
+        }
+
+        else(semana < 1 && semana > 7)
+        {
+            System.out.printf( + semana + ", não corresponde a nada. \n");
         }
     
     }
---
+++ NomeEIdade.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Nome&Idade {
+
+    public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    int I1;
+    String nome;
+
+    System.out.println("Digite seu nome: ");
+    nome = input.next();
+
+    System.out.println("Informe a sua idade: ");
+    I1 = input.nextInt();
+
+    
+    if (I1 >= 0 && I1 <= 12)
+    {
+    System.out.print("Olá " + nome + ",sua faixa etária se encontra entre 0 a 12 anos: Criança");
+
+    } else if (I1 >= 13 && I1 <= 17) {
+    System.out.print("Olá " + nome + ",sua faixa etária se encontra entre 13 a 17 anos: Adolescente");
+
+    } else if (I1 >= 18 && I1 <= 59) {
+    System.out.print("Olá " + nome + ",sua faixa etária se encontra entre 18 a 59 anos: Adulto ");
+
+    } else if (I1 >= 60) {
+    System.out.print("Mais de 60: Idoso \n");
+    }
+
+    else if (I1 <= 0) {
+    System.out.print("ERRO \n");
+    }
+    
+    }
+
+    
+}
